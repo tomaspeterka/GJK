3ab9c6035750d468dae0724b9ca09f3513408414
Tomáš Peterka <125251429+tomaspeterka@users.noreply.github.com>
2023-09-29 10:59:16 +0200

    Update L03-Mycalculator.py
    Adding of what happens when user doesn't input numbers

--- L03-Mycalculator.py
+++ L03-Mycalculator.py
@@ -7,14 +7,27 @@ print("3.Multiply")
 print("4.Divide")
 
 while True:
-        # take input from the user
+        
         choice = input("Enter choice(1/2/3/4): ")
 
-        # check if choice is one of the four options
         if choice in ('1', '2', '3', '4'):
             
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
+            #num1 = float(input("Enter first number: "))
+            #num2 = float(input("Enter second number: "))
+
+            # This part adds Value errors
+            while True:
+                try:
+                    num1 = float(input("Enter first number: "))
+                    break
+                except ValueError:
+                    print("Invalid Input. Try again...")
+            while True:
+                try:
+                    num2 = float(input("Enter second number: "))
+                    break
+                except ValueError:
+                    print("Invalid Input. Try again...")
 
             if num1 + num2 > 10:
                  print ("Are you crazy? This will take me at least 4 minutes to count.")
